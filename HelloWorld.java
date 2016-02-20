b17ce6322015a814523c9443710d009cdccdc597
congiolac <duongnv@mog.net>
2016-02-20 16:38:34 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,9 @@ public class HelloWorld{
 
   public static void main(String[] args) throws Exception{
  
-    System.out.println("HelloWorld");
+    	System.out.println("HelloWorld");
 	System.out.println("fuck");
-
+	System.out.println("fuck1111");
   }
 
 }
