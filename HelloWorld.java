81be5b94a5bcda0efe5314f130cff4f54a8d9799
congiolac <duongnv@mog.net>
2016-02-22 17:38:53 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld{
   public static void main(String[] args) throws Exception{
  
     	System.out.println("Mai");
-	System.out.println("moi");
+	System.out.println("moi xxx");
 	
   }
 
